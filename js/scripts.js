c83a92531c594b64a2533b45d1f38e112118a16b
Simon Thompson <simonct.sct@gmail.com>
2021-01-23 13:02:51 +0800

    UI and algorithm complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,48 @@
-// Add JS
-// select all of the text in the main-input field with .value
-// create a cleanup function to remove all of the \s \n
-// special chars etc.
-// create array of all words separated by space
-// create hash with word frequencies
-// pass each key (word) and it's frequency (value) to the ui
-
-
-
-// function test() {
-//   let rawText = document.getElementById("main-input").value
-//   console.log(rawText)
-// }
-
 function cleanText() {
-  let raw = document.getElementById("main-input").value.toLowerCase()
-  clean = raw.replace(/\s{2,}/g, " ").replace(/[^a-zA-Z\s]/g, "")
-  return clean // returns string
+  let raw = document.getElementById("main-input").value.toLowerCase();
+  clean = raw.replace(/\s{2,}/g, " ").replace(/[^a-zA-Z ]/g, "");
+  return clean; // returns string
 }
 
-function convertCleanTextToArray() {
-  clean = cleanText()
-  return clean.split(" ") // returns array
+function convertCleanTextToSingleWordsArray() {
+  clean = cleanText();
+  return clean.split(" "); // returns array
 }
 
 function buildWordFrequencyObject() {
   frequencyObject = {}
-  textArray = convertCleanTextToArray()
+  textArray = convertCleanTextToSingleWordsArray()
+  for (i = 0; i < textArray.length; i++) {
+    if (frequencyObject.hasOwnProperty(textArray[i])) {
+      frequencyObject[textArray[i]]++;
+    } else {
+      frequencyObject[textArray[i]] = 1;
+      }
+    }
+    return frequencyObject;
+  }
+
+function sortFrequencyObjectToArray() {
+  let frequencyObject = buildWordFrequencyObject();
+  let sortedArray = [];
+  for (word in frequencyObject) {
+      sortedArray.push([word, frequencyObject[word]]);
+  };
+  sortedArray.sort(function(a, b) {
+      return b[1] - a[1];
+  });
+  return sortedArray;
+}
+
+function printWordsToPage() {
+  sortedArray = sortFrequencyObjectToArray();
+  for (i = 0; i < sortedArray.length; i++) {
+    if (sortedArray[i][0] == "") {
+      continue
+    } else {
+      p = document.createElement("p");
+      p.innerText = `${sortedArray[i][0]}: ${sortedArray[i][1]}`;
+      document.getElementById("results-area").appendChild(p);
+    }
+  }
 }
